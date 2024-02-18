6dfd6586eba273ae52831884b5839b0937cb79b0
diegoalexandre.teixeira@gmail.com <diegoalexandre.teixeira@gmail.com>
2024-02-18 17:10:28 -0300

    first commit

---
+++ src/br/com/diegoAlexandre/tarefa1.java
@@ -0,0 +1,16 @@
+package br.com.diegoAlexandre;
+
+public class tarefa1 {
+
+	public static void main(String[] args) {
+		
+		
+		int num = 10;
+		System.out.println(num);
+		
+		num = Integer.valueOf(12);
+		System.out.println(num);
+
+	}
+
+}
